1d0ee41cfbba87f147a2a1ab5c324d4abb609682
Avinash Jha <javinash087@gmail.com>
2020-10-22 20:09:28 +0530

    Added images and posts folder in src folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,26 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve : `gatsby-source-filesystem`,
+      options : {
+        name : `pages`,
+        src : `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve : `gatsby-source-filesystem`,
+      options : {
+        name : `posts`,
+        src : `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve : `gatsby-source-filesystem`,
+      options : {
+        name : `images`,
+        src : `${__dirname}/src/images`,
+      },
+    },
+  ],
 }
